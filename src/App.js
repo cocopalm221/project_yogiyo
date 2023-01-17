e07249a6fb3c7bba825df21d3d2da21b60b39a84
h.young <cocotree221@gmail.com>
2023-01-17 17:38:36 +0900

    App.js / footer import

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Header from "./components/Header";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/signup" element={<SignUp />} />
         </Routes>
       </div>
+      <Footer />
     </Router>
   );
 }
