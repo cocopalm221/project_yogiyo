f660311c28b67f0337cd34c832197cfca00979b4
bmj <bmj44571627@gmail.com>
2023-01-31 17:53:49 +0900

    토큰저장소...

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./userslice";
+import tokenReducer from './Auth';
 
 
 const store = configureStore({
   reducer: {
     userInfo: userSlice,
+    authToken:tokenReducer,
   },
 });
 
